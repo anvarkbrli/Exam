723cde7228844ca8e27b721e0ed1dbcde72d61a7
cff <cff@code.local>
2026-06-11 12:38:28 +0400

    last backup and fixes

--- Exam/Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
+++ Exam/Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4099cdb6672e552d862eac341efd2465626b11")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
